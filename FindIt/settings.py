4c21a3535ed9261f026a7b10de589729db9a8f17
SebishiPro <sebastian_vega12@hotmail.com>
2022-12-06 13:35:14 -0700

    final 2.6

--- FindIt/settings.py
+++ FindIt/settings.py
@@ -154,7 +154,7 @@ STATIC_TMP = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = "BackEnd.User"
-CSRF_TRUSTED_ORIGINS = ['finditback.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['finditback.onrender.com', 'https://frabjous-sorbet-8e8dc1.netlify.app/']
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
